6eb4aab2e4e457522fc62a8c1d52aaa96a60c192
yu miao <miaoy@hongtu56.com>
2019-04-02 10:51:55 +0800

    mock 框架配置

---
+++ mock/index.js
@@ -0,0 +1,9 @@
+const proxy = {
+    'GET /api/product/list': [{"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2}, {
+        "id": 2,
+        "title": "H&M T-Shirt White",
+        "price": 10.99,
+        "inventory": 10
+    }]
+};
+module.exports = proxy;
